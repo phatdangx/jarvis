a2a4e737d3c8370b30fe556ef579f3fd7df329f8
phaatdang <phatdang.work@gmail.com>
2023-08-18 23:29:46 -0700

    feat(hr-command): add HR commands

---
+++ business/hr.py
@@ -0,0 +1,7 @@
+def get_employee_info(employee_id):
+    message = "Employee {} info...".format(employee_id)
+    # Implement the real logic here. It can be query to database or requesting another RESTful API
+    return message
+
+def view_employee_pto(employee_id):
+    message = "Employee {} has requested PTO. Reason: Vacation".format(employee_id)
--- cmd/hr.py
+++ cmd/hr.py
@@ -1,4 +1,8 @@
 from telegram.ext import CommandHandler
+from telegram.parsemode import ParseMode
+from utils.constants import HR, HR_HELP_DICT
+from utils.decorator import requires_user_group
+from business.hr import *
 
 class HrCommand(object):
     """
@@ -13,4 +17,66 @@ class HrCommand(object):
         """
             Registers the admin commands.
         """
-        self.__dispatcher.add_handler(CommandHandler("help", self.__user_cmd_helps))
+        self.__dispatcher.add_handler(CommandHandler("hrhelp", self.__user_cmd_helps))
+        self.__dispatcher.add_handler(CommandHandler("info", self.__info))
+        self.__dispatcher.add_handler(CommandHandler("viewpto", self.__viewpto))
+
+    
+    @staticmethod
+    @requires_user_group(HR)
+    def __user_cmd_helps(update, context):
+        """Command to show help
+        """
+        bot = context.bot
+        help_dictionary = HR_HELP_DICT
+        message = "*JARVIS* support HR those commands:\n\n"
+        for key in help_dictionary.keys():
+            message += help_dictionary[key]
+        bot.sendMessage(
+            chat_id=update.message.chat_id,
+            text=message,
+            parse_mode=ParseMode.MARKDOWN_V2,
+            reply_to_message_id=update.message.message_id
+        )
+
+    @staticmethod
+    @requires_user_group(HR)
+    def __info(update, context):
+        """Command to get information 
+        """
+        bot = context.bot
+        args = context.args
+        message = ""
+        if len(args) > 1:
+            message = "Syntax: info <employee_id>"
+        else:
+            employee_id = args[0]
+            message = get_employee_info(employee_id)
+
+        bot.sendMessage(
+            chat_id=update.message.chat_id,
+            text=message,
+            parse_mode=ParseMode.MARKDOWN,
+            reply_to_message_id=update.message.message_id
+        )
+
+    @staticmethod
+    @requires_user_group(HR)
+    def __viewpto(update, context):
+        """Command to view employee PTO request
+        """
+        bot = context.bot
+        args = context.args
+        message = ""
+        if len(args) > 1:
+            message = "Syntax: viewpto <employee_id>"
+        else:
+            employee_id = args[0]
+            message = view_employee_pto(employee_id)
+
+        bot.sendMessage(
+            chat_id=update.message.chat_id,
+            text=message,
+            parse_mode=ParseMode.MARKDOWN,
+            reply_to_message_id=update.message.message_id
+        )
